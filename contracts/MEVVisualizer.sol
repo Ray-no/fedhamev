74968c056d36fff2b5ed5f3d948578b84584f11f
Ray Allan <128452150+Ray-no@users.noreply.github.com>
2024-04-23 18:50:23 +0300

    Update MEVVisualizer.sol
    Add annotated smart contract with detailed documentation and comments

--- contracts/MEVVisualizer.sol
+++ contracts/MEVVisualizer.sol
@@ -1,52 +1,92 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+/**
+ * @title MEVVisualizer
+ * @dev A smart contract for identifying and visualizing Miner Extractable Value (MEV) opportunities on the Ethereum blockchain.
+ */
 contract MEVVisualizer {
-    address public owner;
-    mapping(address => bool) public authorizedUsers;
-    
+    address public owner; // Address of the contract owner
+    mapping(address => bool) public authorizedUsers; // Mapping of authorized users
+
+    // Structure to store transaction details
     struct Transaction {
-        address sender;
-        address receiver;
-        uint256 amount;
-        uint256 gasPrice;
-        uint256 timestamp;
+        address sender; // Sender address
+        address receiver; // Receiver address
+        uint256 amount; // Transaction amount
+        uint256 gasPrice; // Gas price
+        uint256 timestamp; // Transaction timestamp
     }
-    
-    Transaction[] public transactions;
-    
-    event TransactionAdded(address sender, address receiver, uint256 amount, uint256 gasPrice, uint256 timestamp);
-    event MEVOpportunityIdentified(uint256 transactionId, string opportunityType, uint256 potentialProfit);
-    
+
+    Transaction[] public transactions; // Array to store transactions
+
+    // Events
+    event TransactionAdded(address sender, address receiver, uint256 amount, uint256 gasPrice, uint256 timestamp); // Event emitted when a new transaction is added
+    event MEVOpportunityIdentified(uint256 transactionId, string opportunityType, uint256 potentialProfit); // Event emitted when a MEV opportunity is identified
+
+    /**
+     * @dev Contract constructor
+     * Initializes the contract owner and authorizes the deployer as an authorized user.
+     */
     constructor() {
         owner = msg.sender;
         authorizedUsers[msg.sender] = true;
     }
-    
+
+    /**
+     * @dev Modifier to allow only the owner to execute a function.
+     */
     modifier onlyOwner() {
         require(msg.sender == owner, "Only owner can perform this action");
         _;
     }
-    
+
+    /**
+     * @dev Modifier to allow only authorized users to execute a function.
+     */
     modifier onlyAuthorized() {
         require(authorizedUsers[msg.sender], "Not authorized to perform this action");
         _;
     }
-    
+
+    /**
+     * @dev Adds a new transaction to the transactions array.
+     * Can only be called by authorized users.
+     * @param _sender The sender address of the transaction.
+     * @param _receiver The receiver address of the transaction.
+     * @param _amount The amount of the transaction.
+     * @param _gasPrice The gas price of the transaction.
+     * @param _timestamp The timestamp of the transaction.
+     */
     function addTransaction(address _sender, address _receiver, uint256 _amount, uint256 _gasPrice, uint256 _timestamp) external onlyAuthorized {
         Transaction memory newTransaction = Transaction(_sender, _receiver, _amount, _gasPrice, _timestamp);
         transactions.push(newTransaction);
         emit TransactionAdded(_sender, _receiver, _amount, _gasPrice, _timestamp);
     }
-    
+
+    /**
+     * @dev Authorizes a user to perform actions on the contract.
+     * Can only be called by the contract owner.
+     * @param _user The address of the user to authorize.
+     */
     function authorizeUser(address _user) external onlyOwner {
         authorizedUsers[_user] = true;
     }
-    
+
+    /**
+     * @dev Revokes authorization of a user from performing actions on the contract.
+     * Can only be called by the contract owner.
+     * @param _user The address of the user to revoke authorization from.
+     */
     function revokeAuthorization(address _user) external onlyOwner {
         authorizedUsers[_user] = false;
     }
-    
+
+    /**
+     * @dev Identifies MEV opportunities in the transactions array.
+     * Calculates potential profits based on gas optimization and arbitrage opportunities.
+     * Can only be called by the contract owner.
+     */
     function identifyMEVOpportunities() external onlyOwner {
         for (uint256 i = 0; i < transactions.length; i++) {
             Transaction storage tx = transactions[i];
@@ -70,24 +110,44 @@ contract MEVVisualizer {
             }
         }
     }
-    
+
+    /**
+     * @dev Checks if the transaction has a gas optimization opportunity.
+     * @param _gasPrice The gas price of the transaction.
+     * @return A boolean indicating whether there is a gas optimization opportunity.
+     */
     function isGasOptimizationOpportunity(uint256 _gasPrice) internal pure returns (bool) {
-      
         return _gasPrice > 50000000000; // 50 Gwei in Wei
     }
-    
+
+    /**
+     * @dev Calculates potential profit from gas optimization.
+     * @param _gasPrice The gas price of the transaction.
+     * @return The potential profit from gas optimization in Wei.
+     */
     function calculateGasOptimizationProfit(uint256 _gasPrice) internal pure returns (uint256) {
-       
         return 10000000000000000; // 0.01 ETH in Wei
     }
-    
+
+    /**
+     * @dev Checks if the transaction has an arbitrage opportunity.
+     * @param _sender The sender address of the transaction.
+     * @param _receiver The receiver address of the transaction.
+     * @param _amount The amount of the transaction.
+     * @return A boolean indicating whether there is an arbitrage opportunity.
+     */
     function isArbitrageOpportunity(address _sender, address _receiver, uint256 _amount) internal pure returns (bool) {
-       
         return _sender != _receiver && _amount >= 1 ether; // Arbitrary conditions for demo
     }
-    
+
+    /**
+     * @dev Calculates potential profit from arbitrage.
+     * @param _sender The sender address of the transaction.
+     * @param _receiver The receiver address of the transaction.
+     * @param _amount The amount of the transaction.
+     * @return The potential profit from arbitrage in Wei.
+     */
     function calculateArbitrageProfit(address _sender, address _receiver, uint256 _amount) internal pure returns (uint256) {
-     
         return 50000000000000000; // 0.05 ETH in Wei
     }
 }
